8e4aee083c4ae9de67813a920cc3a57031c612d5
michellemfcfm <47621493+michellemfcfm@users.noreply.github.com>
2019-10-26 15:55:01 -0500

    Add files via upload
    Practica 7

---
+++ Practica7/SQLQuery5.sql
@@ -0,0 +1,2 @@
+BACKUP DATABASE [Practica 7] TO  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\Backup\Practica 7.bak' WITH NOFORMAT, NOINIT,  NAME = N'Practica 7-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+GO
